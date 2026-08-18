cb18746f817da1b842da337e5fe41c3c4a758052
shraddhadhawale2-stack <shraddhadhawale2@gamil.com>
2026-08-18 20:57:06 +0530

    Update CropVision AI website

--- crop_yield_web/app.py
+++ crop_yield_web/app.py
@@ -10,7 +10,7 @@ model = joblib.load("crop_yield_model.pkl")
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "NEW CROPVISION PAGE"
 
 
 @app.route("/predict", methods=["POST"])
